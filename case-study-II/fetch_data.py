ac51c1e8764df6b9c6da79f2db51e632f7564387
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2022-03-24 16:07:37 +0100

    Update fetch_data.py

--- case-study-II/fetch_data.py
+++ case-study-II/fetch_data.py
@@ -7,7 +7,6 @@ except ImportError:
 import time
 
 AMAZON_REVIEW_URL = ("https://s3.amazonaws.com/fast-ai-nlp/amazon_review_polarity_csv.tgz")
-#"https://doc-0o-9g-docs.googleusercontent.com/docs/securesc/5eqdu74r584cfufeb8vmchjpj1ptksh5/ojmcg877ql586rbsdkeufg26vinb7qa4/1641494025000/07511006523564980941/14553342361444956870/0Bz8a_Dbh9QhbaW12WVVZS2drcnM?resourcekey=0-NlNEsGjcAEPJBwDJtuuZ3Q&e=download&authuser=0&nonce=plg1bg5jjl7bk&user=14553342361444956870&hash=ge521kq0qla99g8a31uhptlg0oocnbe5")
 AMAZON_REVIEW_ARCHIVE_NAME = "amazon_review_polarity_csv.tar.gz"
 
 
